f3c6511973c26b0537e63140f059c5af3d36c10a
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-07 17:37:35 +0200

    Configuration for token_auth

--- config/config.exs
+++ config/config.exs
@@ -19,6 +19,37 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :phoenix_token_auth,
+  user_model: Catcaluser.User,           # ecto model used for authentication
+  repo: Catcaluser.Repo,                 # ecto repo
+  crypto_provider: Comeonin.Bcrypt,      # crypto provider for hashing passwords/tokens. see http://hexdocs.pm/comeonin/
+  # secret string used to sign the authentication token
+  token_secret: "the_very_secret_token", 
+  # minutes from login until a token expires
+  token_validity_in_minutes: 7 * 24 * 60, 
+  # sender address of emails sent by the app   
+  email_sender: "catcaluser@catcal.zone", 
+  # function returning the subject of a welcome email
+  welcome_email_subject: fn user -> "Hello #{user.email}" end,
+  # function returning the body of a welcome email
+  welcome_email_body: fn user, confirmation_token -> confirmation_token end,
+  # function returning the subject of a welcome email
+  password_reset_email_subject: fn user -> "Hello #{user.email}" end,
+  # function returning the body of a welcome email
+  password_reset_email_body: fn user, reset_token -> reset_token end,
+  # function returning the subject of the email sent when the users changes his email address
+  new_email_address_email_subject: fn user -> "Hello #{user.email}" end, 
+  # function returning the body of the email sent when the users changes his email address
+  new_email_address_email_body: 
+  	fn user, confirmation_token -> confirmation_token end, 
+  # domain of your mailgun account
+  mailgun_domain: "catcal.zone",
+  # secret key of your mailgun account
+  mailgun_key: "secret", 
+  # function receiving and returning the changeset for a user on registration and 
+  # when updating the account. This is the place to run custom validations.
+  user_model_validator: fn changeset -> changeset end
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
