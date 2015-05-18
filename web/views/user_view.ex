b5acdaf72131d830ba1a29cd9ac643034bd98088
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-18 21:22:41 +0200

    render users with json prefix via jsonsuserview

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,3 +1,9 @@
 defmodule Catcaluser.UserView do
   use Catcaluser.Web, :view
+
+  @doc """
+  Render all entries with a `json` prefix via `JsonUserView`.
+  """
+  def render(entry = "json" <> _, js), do: Catcaluser.JsonUserView.render(entry, js)
+  
 end
