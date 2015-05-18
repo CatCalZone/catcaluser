e0fdb24ae516f99c7dae8008ccd8cc06d05fb291
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-18 21:29:46 +0200

    proper rendering of JsonUser

--- web/views/json_user_view.ex
+++ web/views/json_user_view.ex
@@ -1,6 +1,9 @@
 defmodule Catcaluser.JsonUserView do
   use Catcaluser.Web, :view
 
+  alias Catcaluser.JsonUser
+  alias Catcaluser.User
+
   def render("index.json", %{jsonusers: jsonusers}) do
     IO.puts "Render may jsonusers: #{inspect jsonusers}"
     %{data: render_many(jsonusers, "json_user.json")}
@@ -10,8 +13,8 @@ defmodule Catcaluser.JsonUserView do
     %{data: render_one(json_user, "json_user.json")}
   end
 
-  def render("json_user.json", %{json_user: json_user}) do
-    %{id: json_user.id}
+  def render("json_user.json", %{json_user: json_user = %JsonUser{}}) do
+    %{id: json_user.id, email: json_user.email, name: json_user.name}
   end
 
 end
