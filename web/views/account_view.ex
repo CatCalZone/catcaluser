61248add7356336dd18f2b2266c602d45dadfac7
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-19 09:25:41 +0200

    render json entries in the JsonAccountView

--- web/views/account_view.ex
+++ web/views/account_view.ex
@@ -1,3 +1,9 @@
 defmodule Catcaluser.AccountView do
   use Catcaluser.Web, :view
+
+  @doc """
+  Render all entries with a `json` prefix via `JsonAccountView`.
+  """
+  def render(entry = "json" <> _, js), do: Catcaluser.JsonAccountView.render(entry, js)
+  
 end
