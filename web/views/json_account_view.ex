d2699fa83f6c0abd6336c8655082c511b86a7f70
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-19 09:26:37 +0200

    again the json_account vs. account dilemma

--- web/views/json_account_view.ex
+++ web/views/json_account_view.ex
@@ -1,15 +1,22 @@
 defmodule Catcaluser.JsonAccountView do
   use Catcaluser.Web, :view
 
-  def render("index.json", %{jsonaccountss: jsonaccountss}) do
-    %{data: render_many(jsonaccountss, "json_account.json")}
+  alias Catcaluser.Account
+
+  def render("index.json", %{jsonaccounts: jsonaccounts}) do
+    %{data: render_many(jsonaccounts, "json_account.json")}
   end
 
   def render("show.json", %{json_account: json_account}) do
+    # IO.puts "Render one json_account: #{inspect json_account}"
     %{data: render_one(json_account, "json_account.json")}
   end
 
   def render("json_account.json", %{json_account: json_account}) do
+    render("json_account.json", %{account: json_account})
+  end
+  def render("json_account.json", %{account: json_account}) do
     %{id: json_account.id}
   end
+
 end
