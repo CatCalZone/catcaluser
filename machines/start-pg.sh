b5221dd2367b354cec2f6095e21213b7563b15fe
Klaus Alfert <klaus.alfert@googlemail.com>
2015-05-31 21:32:51 +0200

    let the postgres db run under user catcal

--- machines/start-pg.sh
+++ machines/start-pg.sh
@@ -2,12 +2,9 @@
 #
 # starts a new postgres container
 
-pg_user=postgres
-pg_passwd=postgres
+pg_user=catcal
+pg_passwd=catcal
 
 sudo docker run --name catcaluser_db \
 	-e POSTGRES_PASSWORD=$pg_passwd -e  POSTGRES_USER=$pg_user \
 	-d -p 15432:5432 postgres
-
-#echo "To (re)create the database do:"
-#echo psql --host 127.0.0.1 --username=$pg_user -f sql/microimage.sql
