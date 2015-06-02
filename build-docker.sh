04592c960842b4bbeb37dc7d4862adba21628462
Klaus Alfert <klaus.alfert@googlemail.com>
2015-06-02 13:02:36 +0200

    Load the file from github if -r is given as parameter

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-
+# set -x
 VERSION=0.0.1
-cp rel/catcaluser/catcaluser-$VERSION.tar.gz machines/phoenix/catcaluser.tar.gz
+URL=https://github.com/CatCalZone/catcaluser/releases/download/v0.0.1/catcaluser-0.0.1.tar.gz
+
+DEST=machines/phoenix/catcaluser.tar.gz
+
+if [ "$1" = "-r" ] 
+then
+	curl --location -o $DEST $URL 
+else
+	cp rel/catcaluser/catcaluser-$VERSION.tar.gz $DEST
+fi
+
 
 cd machines/phoenix
 sudo docker build -t catcal/catcaluser .
