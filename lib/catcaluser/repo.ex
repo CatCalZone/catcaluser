a8af66545dba27f9bd94fe49a42a4e43fd4e577e
Klaus Alfert <klaus.alfert@googlemail.com>
2015-06-14 18:48:52 +0200

    Monitoring Ecto with Exometer

--- lib/catcaluser/repo.ex
+++ lib/catcaluser/repo.ex
@@ -1,3 +1,23 @@
 defmodule Catcaluser.Repo do
   use Ecto.Repo, otp_app: :catcaluser
+
+  alias :exometer, as: Exometer
+
+  @doc """
+  Override the logging of Ecto and forwards metrics towards Exometer
+  """
+  def log({atom, cmd, params}, fun) do
+    before_time = :os.timestamp
+ 
+    result = super({atom, cmd, params}, fun)
+ 
+    after_time = :os.timestamp
+    diff = :timer.now_diff after_time, before_time
+    :ok = Exometer.update ~w(catcaluser ecto query_exec_time)a, diff / 1_000
+    :ok = Exometer.update ~w(catcaluser ecto query_count)a, 1
+ 
+    result
+  end
+  def log(atom, fun), do: super(atom, fun)
+  
 end
